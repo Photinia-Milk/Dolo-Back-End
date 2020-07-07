5ab7aebecff561004a644f176f91b2d1b85a0cf0
Hongyi Long <lhy159840@outlook.com>
2020-07-07 10:42:50 +0800

    =init back-end

---
+++ back-end/src/test/java/sjtu/couse/CouseApplicationTests.java
@@ -0,0 +1,13 @@
+package sjtu.couse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
