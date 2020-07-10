a42eb7e65ad1077fd027c36c81beec7a95d1eefc
Jinke Liu <lytgyqljk@163.com>
2020-07-10 12:02:05 +0800

    chore: create the DAO

--- src/main/java/sjtu/dolo/mapper/StudentMapper.java
+++ src/main/java/sjtu/dolo/mapper/StudentMapper.java
@@ -1,5 +1,14 @@
 package sjtu.dolo.mapper;
 
-public interface StudentMapper {
+import sjtu.dolo.model.Course;
+import sjtu.dolo.model.Section;
+import sjtu.dolo.model.Takes;
 
+import java.util.List;
+
+public interface StudentMapper {
+     List<Section> getAllSection();
+     String addTakes(Takes takes);
+     String delTakes(Takes takes);
+     List<Takes> getAllTakes(String studentID);
 }
