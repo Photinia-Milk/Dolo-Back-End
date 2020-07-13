69b4a76f18a88ac05e5f3c129803777ee7cb4fa1
Jinke Liu <lytgyqljk@163.com>
2020-07-13 17:41:40 +0800

    chore: solve the dependency problem

--- src/test/java/sjtu/dolo/CodeGenerator.java
+++ src/test/java/sjtu/dolo/CodeGenerator.java
@@ -1,7 +1,10 @@
 package sjtu.dolo;
 
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+
 public class CodeGenerator {
     public static void main(String[] args){
-
+//        AutoGenerator autoGenerator =
     }
 }
