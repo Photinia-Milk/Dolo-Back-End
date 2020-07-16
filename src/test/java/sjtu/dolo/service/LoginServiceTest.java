1a5defaf5b20e5cdf149ebcd31b6503fbb451cbb
Xingyu <sdjm_wxy@sjtu.edu.cn>
2020-07-16 14:04:52 +0800

    fix: modify the loginServiceTest

--- src/test/java/sjtu/dolo/service/LoginServiceTest.java
+++ src/test/java/sjtu/dolo/service/LoginServiceTest.java
@@ -1,4 +1,25 @@
 package sjtu.dolo.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import sjtu.dolo.mapper.UserAuthMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LoginServiceTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+    @Autowired
+    private StudentService studentService;
+
+    @MockBean
+    private UserAuthMapper userAuthMapper;
 }
