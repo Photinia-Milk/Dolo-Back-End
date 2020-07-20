42d5bfcc8a3a7c039581074ff4ae4c071cc3124e
Jinke Liu <lytgyqljk@163.com>
2020-07-20 10:37:35 +0800

    chore: finish the unit test

--- src/test/java/sjtu/dolo/mapper/TakesMapperTest.java
+++ src/test/java/sjtu/dolo/mapper/TakesMapperTest.java
@@ -1,6 +1,11 @@
 package sjtu.dolo.mapper;
 
+import org.junit.Test;
 import sjtu.dolo.CourseApplicationTests;
 
 public class TakesMapperTest extends CourseApplicationTests {
+    @Test
+    public void getTakesTest(){
+
+    }
 }
